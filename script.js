153729bbc6a87735e53ccc00c051d29652f572ee
김재원 <stolo@MacBookstolo.local>
2025-07-20 19:27:48 +0900

    DOM으로 감싸기

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ function sayHello() {
     alert("안녕하세요! 방문해주셔서 감사합니다 😊");
 }
 
-// localStorage를 이용한 방문자 수 세기
-let visits = localStorage.getItem('visitCount');
+document.addEventListener('DOMContentLoaded', () => {
+    // localStorage를 이용한 방문자 수 세기
+    let visits = localStorage.getItem('visitCount');
 
-if (visits) {
-    visits = parseInt(visits) + 1;  // 방문 횟수 1 증가
-} else {
-    visits = 1;  // 첫 방문이면 1로 시작
-}
+    if (visits) {
+        visits = parseInt(visits) + 1;  // 방문 횟수 1 증가
+    } else {
+        visits = 1;  // 첫 방문이면 1로 시작
+    }
 
-localStorage.setItem('visitCount', visits);
+    localStorage.setItem('visitCount', visits);
 
-// 방문 횟수 화면에 표시
-document.getElementById('visitor-count').textContent = visits + '회 방문했습니다.';
+    // 방문 횟수 화면에 표시
+    document.getElementById('visitor-count').textContent = visits + '회 방문했습니다.';
+});
